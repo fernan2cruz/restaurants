5435b074f60368586efe7e837ca29d1448934f1f
Fernando Cruz <fernando_acz@yahoo.com>
2021-04-13 15:30:51 -0400

    Agregamos el logo al proyecto y terminamos la pantalla de GuestAccount
    Tutorial 64 Cap 35

--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,13 +1,56 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image} from 'react-native'
+import { Button } from 'react-native-elements'
 import Loading from '../../components/Loading'
 
 export default function UserGuest() {
     return (
-        <View>
-            <Text>UserGuest.....</Text>            
-        </View>
+       <ScrollView
+            centerContent
+            style={styles.viewBody}
+       >
+           <Image
+                source={require("../../assets/restaurant-logo.png")}
+                resizeMode="contain"
+                style={styles.image}
+           />
+           <Text style={styles.title}>Consulta tu perfil en Restaurants</Text>
+           <Text style={styles.description}>como describirias tu mejor restaruante? Busca y visualiza los mejores restaruantes Busca y visualiza los mejores restaruantes
+           Busca y visualiza los mejores restaruantesBusca y visualiza los mejores restaruantes</Text>
+           <Button 
+                title="Ver tu Perfil"
+                buttonStyle={styles.button}
+                onPress={()=> console.log("Click !!!")}
+           >
+
+           </Button>
+       </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody:{
+        marginHorizontal: 30 
+    },
+    image:{
+        height:600,
+        width: "100%",
+        marginBottom:10,
+        textAlign: "center"        
+    },
+    title:{
+        fontWeight:"bold",
+        fontSize:19,
+        marginBottom:10,
+        textAlign:"center"
+    },
+    description:{
+        textAlign: "justify", 
+        marginBottom: 20, 
+        color: "#a65273"
+    },
+    button:{
+        backgroundColor:"#442484"
+    }
+
+})
